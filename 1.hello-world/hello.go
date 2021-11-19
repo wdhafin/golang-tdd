f17d95edb71bf29138569ea58edb61891b5c067b
Muhammad Dhafin Rizqullah <work.dhafin@gmail.com>
2021-11-19 14:07:28 +0700

    [refactor] handle if name is empty

--- 1.hello-world/hello.go
+++ 1.hello-world/hello.go
@@ -1,5 +1,10 @@
 package hello
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
--- 1.hello-world/hello_test.go
+++ 1.hello-world/hello_test.go
@@ -3,10 +3,20 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
